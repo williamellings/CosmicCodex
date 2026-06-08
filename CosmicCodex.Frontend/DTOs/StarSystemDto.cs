4b684eba8151173f403d4f54c727c11b2f7e583b
williamellings <wille.ellings@hotmail.com>
2026-06-08 17:06:35 +0200

    Fixing styke

---
+++ CosmicCodex.Frontend/DTOs/StarSystemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CosmicCodex.Frontend.DTOs
+{
+    public class StarSystemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
