bc738a0ce794ff5319e51c66af28df57b6fe7971
williamellings <wille.ellings@hotmail.com>
2026-06-08 16:01:27 +0200

    Implement Generic Repository, specific repositories and services

---
+++ CosmicCodex.Api/Repositories/StarSystemRepository.cs
@@ -0,0 +1,21 @@
+﻿using CosmicCodex.Api.Data;
+using CosmicCodex.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CosmicCodex.Api.Repositories
+{
+    public class StarSystemRepository : GenericRepository<StarSystem>, IStarSystemRepository
+    {
+        public StarSystemRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<StarSystem?> GetWithPlanetsAsync(int id)
+        {
+            return await _context.StarSystems
+                .Include(s => s.Planets)
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+    }
+}
